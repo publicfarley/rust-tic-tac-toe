3a466b098c3fc467f0cf69afd42f58bcaac574b2
Farley Caesar <publicfarley@gmail.com>
2024-12-12 12:33:05 -0500

    - Updated `place_piece` function to mutate the passed in game board, instead of returning a new one. - addressed clippy comments

--- src/game_model.rs
+++ src/game_model.rs
@@ -1,14 +1,13 @@
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub enum CellState {
-    Empty,
+pub enum Piece {
     X,
     O,
 }
 
-impl CellState {
-    pub const fn all_cases() -> &'static [Self] {
-        &[Self::Empty, Self::X, Self::O]
-    }
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum CellState {
+    Empty,
+    Owned(Piece),
 }
 
 impl Default for CellState {
@@ -19,11 +18,28 @@ impl Default for CellState {
 
 pub type GameBoard = [[CellState; 3]; 3];
 
-fn place_piece(coordinate: (usize, usize), piece: &CellState, board: &GameBoard) -> GameBoard {
-    let mut new_game_board = board.clone();
-    new_game_board[coordinate.0][coordinate.1] = piece.to_owned();
+pub fn place_piece(piece: &Piece, coordinate: (usize, usize), board: &mut GameBoard) {
+    board[coordinate.0][coordinate.1] = CellState::Owned(piece.to_owned());
+}
 
-    return new_game_board;
+pub fn determine_winner_of_line(line: &[CellState; 3]) -> Option<&Piece> {
+    let first_piece = match &line[0] {
+        CellState::Owned(piece) => Some(piece),
+        CellState::Empty => None,
+    };
+
+    let distinguished_piece = first_piece?; // returns None from the function if first_piece doesn't exist
+
+    if line
+        .iter()
+        .filter(|&cell| matches!(cell, CellState::Owned(piece) if piece == distinguished_piece))
+        .count()
+        == line.len()
+    {
+        Some(distinguished_piece)
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -31,12 +47,65 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_initial_board_is_empty() {
+        let game_board = GameBoard::default();
+
+        for row in &game_board {
+            for cell in row {
+                assert_eq!(*cell, CellState::Empty);
+            }
+        }
+    }
+
+    #[test]
     fn test_place_x() {
         let mut game_board = GameBoard::default();
-        game_board[0][0] = CellState::X;
+        game_board[0][0] = CellState::Owned(Piece::X);
 
-        let game_board2 = GameBoard::default();
-        let result_of_placing_x = place_piece((0, 0), &CellState::X, &game_board2);
-        assert_eq!(game_board[0][0], result_of_placing_x[0][0]);
+        let mut game_board2 = GameBoard::default();
+        place_piece(&Piece::X, (0, 0), &mut game_board2);
+
+        assert_eq!(game_board[0][0], game_board2[0][0]);
+    }
+
+    #[test]
+    fn test_place_o() {
+        let mut game_board = GameBoard::default();
+        game_board[0][0] = CellState::Owned(Piece::O);
+
+        let mut game_board2 = GameBoard::default();
+        place_piece(&Piece::O, (0, 0), &mut game_board2);
+        assert_eq!(game_board[0][0], game_board2[0][0]);
+    }
+
+    #[test]
+    fn test_empty_row_has_no_winner() {
+        let game_board = GameBoard::default();
+
+        assert_eq!(None, determine_winner_of_line(&game_board[0]));
+    }
+
+    #[test]
+    fn test_full_row_of_x_has_winner_x() {
+        let mut game_board = GameBoard::default();
+        let x = &Piece::X;
+
+        place_piece(x, (0, 0), &mut game_board);
+        place_piece(x, (0, 1), &mut game_board);
+        place_piece(x, (0, 2), &mut game_board);
+
+        assert_eq!(Some(x), determine_winner_of_line(&game_board[0]));
+    }
+
+    #[test]
+    fn test_full_row_of_o_has_winner_o() {
+        let mut game_board = GameBoard::default();
+        let o = &Piece::O;
+
+        place_piece(o, (0, 0), &mut game_board);
+        place_piece(o, (0, 1), &mut game_board);
+        place_piece(o, (0, 2), &mut game_board);
+
+        assert_eq!(Some(o), determine_winner_of_line(&game_board[0]));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 mod game_model;
 
-use crate::game_model::{CellState, GameBoard};
+use crate::game_model::{determine_winner_of_line, place_piece, GameBoard, Piece};
 
 fn main() {
-    for state in CellState::all_cases() {
-        println!("Hello, cell state! {state:#?}");
-    }
-
-    let game_board: GameBoard = Default::default();
+    let mut game_board: GameBoard = Default::default();
     println!("Hello game board: {game_board:#?}");
 
-    println!("{:#?}", game_board[0][0]);
-    println!("{:#?}", game_board[0][0]);
+    place_piece(&Piece::O, (0, 0), &mut game_board);
+    println!("game_board[0]: {:#?}\n", game_board[0]);
+
+    println!("Row 0: ");
+    println!("Winner: {:#?}", determine_winner_of_line(&game_board[0]));
+
+    let mut game_board2: GameBoard = Default::default();
+
+    for column in 0..=2 {
+        place_piece(&Piece::X, (0, column), &mut game_board2);
+    }
+
+    println!("game_board[0]: {:#?}\n", game_board2[0]);
+
+    println!("Row 0: ");
+    println!("Winner: {:#?}", determine_winner_of_line(&game_board2[0]));
 }
