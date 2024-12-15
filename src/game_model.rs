e1514e92e392c7a2e4758a291999d8652fbe011e
Farley Caesar <publicfarley@gmail.com>
2024-12-15 11:22:46 -0500

    Added code to determine from a board if there is a winning line

--- src/game_model.rs
+++ src/game_model.rs
@@ -1,3 +1,5 @@
+use std::ops::RangeInclusive;
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Piece {
     X,
@@ -50,6 +52,68 @@ pub fn determine_winner_of_line(line: &[CellState; 3]) -> Option<&Piece> {
         None
     }
 }
+
+const CELL_IDS: RangeInclusive<usize> = 0..=2;
+
+pub fn is_winner(board: &GameBoard) -> bool {
+    for row_index in CELL_IDS {
+        let row = &board[row_index];
+
+        if determine_winner_of_line(&row).is_some() {
+            return true;
+        }
+    }
+
+    for column_index in CELL_IDS {
+        let column: &[CellState; 3] = &[
+            board[0][column_index].clone(),
+            board[1][column_index].clone(),
+            board[2][column_index].clone(),
+        ];
+
+        if determine_winner_of_line(column).is_some() {
+            return true;
+        }
+    }
+
+    let column_0_diagonal: &[CellState; 3] = &[
+        board[0][0].clone(),
+        board[1][1].clone(),
+        board[2][2].clone(),
+    ];
+
+    if determine_winner_of_line(column_0_diagonal).is_some() {
+        return true;
+    }
+
+    let column_2_diagonal: &[CellState; 3] = &[
+        board[0][2].clone(),
+        board[1][1].clone(),
+        board[2][0].clone(),
+    ];
+
+    if determine_winner_of_line(column_2_diagonal).is_some() {
+        return true;
+    }
+
+    false
+}
+
+// fn grab_columns(board: &GameBoard) -> Vec<Vec<&CellState>> {
+//     let mut columns_vector: Vec<Vec<&CellState>> = Vec::new();
+
+//     for column_index in CELL_IDS {
+//         let column = vec![
+//             &board[0][column_index],
+//             &board[1][column_index],
+//             &board[2][column_index],
+//         ];
+
+//         columns_vector.push(column);
+//     }
+
+//     columns_vector
+// }
 
 #[cfg(test)]
 mod tests {
@@ -133,5 +197,62 @@ mod tests {
 
         assert_eq!(result, Ok(()));
         assert_eq!(Some(o), determine_winner_of_line(&game_board[0]));
+    }
+
+    #[test]
+    fn test_empty_board_has_no_winner() {
+        let game_board = GameBoard::default();
+
+        assert_eq!(false, is_winner(&game_board));
+    }
+
+    #[test]
+    fn test_board_with_a_winning_row_has_a_winner() {
+        let mut game_board = GameBoard::default();
+        let x = &Piece::X;
+
+        let result = place_piece(x, (0, 0), &mut game_board)
+            .and_then(|_| place_piece(x, (0, 1), &mut game_board))
+            .and_then(|_| place_piece(x, (0, 2), &mut game_board));
+
+        assert!(result.is_ok());
+        assert_eq!(true, is_winner(&game_board));
+    }
+
+    #[test]
+    fn test_board_with_a_winning_column_has_a_winner() {
+        let mut game_board = GameBoard::default();
+        let x = &Piece::X;
+
+        let result = place_piece(x, (0, 0), &mut game_board)
+            .and_then(|_| place_piece(x, (1, 0), &mut game_board))
+            .and_then(|_| place_piece(x, (2, 0), &mut game_board));
+
+        assert!(result.is_ok());
+        assert_eq!(true, is_winner(&game_board));
+    }
+
+    #[test]
+    fn test_board_with_a_winning_forward_diagonal_has_a_winner() {
+        let mut game_board = GameBoard::default();
+        let x = &Piece::X;
+
+        for index in CELL_IDS {
+            _ = place_piece(x, (index, index), &mut game_board);
+        }
+
+        assert_eq!(true, is_winner(&game_board));
+    }
+
+    #[test]
+    fn test_board_with_a_winning_backward_diagonal_has_a_winner() {
+        let mut game_board = GameBoard::default();
+        let x = &Piece::X;
+
+        for (_, (row_index, column_index)) in CELL_IDS.zip(CELL_IDS.rev()).enumerate() {
+            _ = place_piece(x, (row_index, column_index), &mut game_board);
+        }
+
+        assert_eq!(true, is_winner(&game_board));
     }
 }
