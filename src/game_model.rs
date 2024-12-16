0933d40dc5c593abff78e1037de0c21e3a97f1ae
Farley Caesar <publicfarley@gmail.com>
2024-12-15 23:28:32 -0500

    Moved to a more Value Oriented architecture for the GameBoard and the functions that operate on it

--- src/game_model.rs
+++ src/game_model.rs
@@ -1,10 +1,10 @@
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Piece {
     X,
     O,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum CellState {
     Empty,
     Owned(Piece),
@@ -16,22 +16,57 @@ impl Default for CellState {
     }
 }
 
-pub type GameBoard = [[CellState; 3]; 3];
-
-pub fn place_piece(
-    piece: &Piece,
-    coordinate: (usize, usize),
-    board: &mut GameBoard,
-) -> Result<(), String> {
-    if matches!(&board[coordinate.0][coordinate.1], CellState::Owned(_)) {
-        return Err(String::from("Spot on board is alrady occupied"));
-    }
-
-    board[coordinate.0][coordinate.1] = CellState::Owned(piece.to_owned());
-    Ok(())
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct GameBoard {
+    pub cells: [[CellState; 3]; 3],
 }
 
-pub fn determine_winner_of_line<'a>(line: &'a [&'a CellState]) -> Option<&'a Piece> {
+impl GameBoard {
+    pub fn new() -> Self {
+        Self {
+            cells: [[CellState::default(); 3]; 3],
+        }
+    }
+
+    pub fn get_cell(&self, row: usize, col: usize) -> Option<&CellState> {
+        self.cells.get(row).and_then(|r| r.get(col))
+    }
+
+    pub fn place_piece(&mut self, piece: &Piece, coordinate: (usize, usize)) -> Result<(), String> {
+        let cell = &self.cells[coordinate.0][coordinate.1];
+
+        if matches!(&cell, CellState::Owned(_)) {
+            return Err(String::from("Spot on board is alrady occupied"));
+        }
+
+        self.cells[coordinate.0][coordinate.1] = CellState::Owned(piece.to_owned());
+        Ok(())
+    }
+
+    pub fn determine_winner(&self) -> Option<Piece> {
+        for row in grab_rows(self) {
+            if let Some(piece) = determine_winner_of_line(&row) {
+                return Some(piece.to_owned());
+            }
+        }
+
+        for column in grab_columns(self) {
+            if let Some(piece) = determine_winner_of_line(&column) {
+                return Some(piece.to_owned());
+            }
+        }
+
+        for diagonal in grab_diagonals(self) {
+            if let Some(piece) = determine_winner_of_line(&diagonal) {
+                return Some(piece.to_owned());
+            }
+        }
+
+        None
+    }
+}
+
+fn determine_winner_of_line<'a>(line: &'a [&'a CellState]) -> Option<&'a Piece> {
     // Lifetimes required here to guarantee that the outgoing type (`Piece`) doesn't
     // outlive the incoming type that it is tied to `CellState`.
 
@@ -54,47 +89,27 @@ pub fn determine_winner_of_line<'a>(line: &'a [&'a CellState]) -> Option<&'a Pie
     }
 }
 
-pub fn determine_winner(board: &GameBoard) -> Option<Piece> {
-    for row in grab_rows(board) {
-        if let Some(piece) = determine_winner_of_line(&row) {
-            return Some(piece.to_owned());
-        }
-    }
-
-    for column in grab_columns(board) {
-        if let Some(piece) = determine_winner_of_line(&column) {
-            return Some(piece.to_owned());
-        }
-    }
-
-    for diagonal in grab_diagonals(board) {
-        if let Some(piece) = determine_winner_of_line(&diagonal) {
-            return Some(piece.to_owned());
-        }
-    }
-
-    None
-}
-
 fn grab_rows(board: &GameBoard) -> Vec<Vec<&CellState>> {
-    let row0: Vec<_> = board[0].iter().collect();
-    let row1: Vec<_> = board[1].iter().collect();
-    let row2: Vec<_> = board[2].iter().collect();
+    let row0: Vec<_> = board.cells[0].iter().collect();
+    let row1: Vec<_> = board.cells[1].iter().collect();
+    let row2: Vec<_> = board.cells[2].iter().collect();
 
     vec![row0, row1, row2]
 }
 
 fn grab_columns(board: &GameBoard) -> Vec<Vec<&CellState>> {
-    let column0: Vec<_> = board.iter().map(|row| &row[0..=0][0]).collect();
-    let column1: Vec<_> = board.iter().map(|row| &row[1..=1][0]).collect();
-    let column2: Vec<_> = board.iter().map(|row| &row[2..=2][0]).collect();
+    let column0: Vec<_> = board.cells.iter().map(|row| &row[0..=0][0]).collect();
+    let column1: Vec<_> = board.cells.iter().map(|row| &row[1..=1][0]).collect();
+    let column2: Vec<_> = board.cells.iter().map(|row| &row[2..=2][0]).collect();
 
     vec![column0, column1, column2]
 }
 
 fn grab_diagonals(board: &GameBoard) -> Vec<Vec<&CellState>> {
-    let left_to_right_diagonal: Vec<_> = vec![&board[0][0], &board[1][1], &board[2][2]];
-    let right_to_left_diagonal: Vec<_> = vec![&board[0][2], &board[1][1], &board[2][0]];
+    let left_to_right_diagonal: Vec<_> =
+        vec![&board.cells[0][0], &board.cells[1][1], &board.cells[2][2]];
+    let right_to_left_diagonal: Vec<_> =
+        vec![&board.cells[0][2], &board.cells[1][1], &board.cells[2][0]];
 
     vec![left_to_right_diagonal, right_to_left_diagonal]
 }
@@ -105,9 +120,9 @@ mod tests {
 
     #[test]
     fn test_initial_board_is_empty() {
-        let game_board = GameBoard::default();
+        let game_board = GameBoard::new();
 
-        for row in &game_board {
+        for row in &game_board.cells {
             for cell in row {
                 assert_eq!(*cell, CellState::Empty);
             }
@@ -116,43 +131,36 @@ mod tests {
 
     #[test]
     fn test_place_x_on_empty_cell() {
-        let mut game_board = GameBoard::default();
-        game_board[0][0] = CellState::Owned(Piece::X);
-
-        let mut game_board2 = GameBoard::default();
-        let result = place_piece(&Piece::X, (0, 0), &mut game_board2);
+        let mut game_board = GameBoard::new();
+        let result = game_board.place_piece(&Piece::X, (0, 0));
 
         assert_eq!(result, Ok(()));
-        assert_eq!(game_board[0][0], game_board2[0][0]);
+        assert_eq!(game_board.cells[0][0], CellState::Owned(Piece::X));
     }
 
     #[test]
     fn test_place_o_on_empty_cell() {
-        let mut game_board = GameBoard::default();
-        game_board[0][0] = CellState::Owned(Piece::O);
-
-        let mut game_board2 = GameBoard::default();
-        let result = place_piece(&Piece::O, (0, 0), &mut game_board2);
+        let mut game_board = GameBoard::new();
+        let result = game_board.place_piece(&Piece::O, (0, 0));
 
         assert_eq!(result, Ok(()));
-        assert_eq!(game_board[0][0], game_board2[0][0]);
+        assert_eq!(game_board.cells[0][0], CellState::Owned(Piece::O));
     }
 
     #[test]
     fn test_place_piece_on_owned_cell() {
-        let mut game_board = GameBoard::default();
-        game_board[0][0] = CellState::Owned(Piece::O);
-        let board_with_only_o_placed = game_board.clone();
+        let mut game_board = GameBoard::new();
+        assert!(game_board.place_piece(&Piece::O, (0, 0)).is_ok());
 
-        let result = place_piece(&Piece::X, (0, 0), &mut game_board);
-
+        let result = game_board.place_piece(&Piece::X, (0, 0));
         assert!(result.is_err());
-        assert_eq!(game_board, board_with_only_o_placed);
+
+        assert_eq!(game_board.cells[0][0], CellState::Owned(Piece::O));
     }
 
     #[test]
     fn test_empty_row_has_no_winner() {
-        let game_board = GameBoard::default();
+        let game_board = GameBoard::new();
         let rows = grab_rows(&game_board);
 
         assert_eq!(None, determine_winner_of_line(&rows[0]));
@@ -160,12 +168,13 @@ mod tests {
 
     #[test]
     fn test_full_row_of_x_has_winner_x() {
-        let mut game_board = GameBoard::default();
+        let mut game_board = GameBoard::new();
         let x = &Piece::X;
 
-        let result = place_piece(x, (0, 0), &mut game_board)
-            .and_then(|_| place_piece(x, (0, 1), &mut game_board))
-            .and_then(|_| place_piece(x, (0, 2), &mut game_board));
+        let result = game_board
+            .place_piece(x, (0, 0))
+            .and_then(|_| game_board.place_piece(x, (0, 1)))
+            .and_then(|_| game_board.place_piece(x, (0, 2)));
 
         assert_eq!(result, Ok(()));
 
@@ -177,12 +186,13 @@ mod tests {
 
     #[test]
     fn test_full_row_of_o_has_winner_o() {
-        let mut game_board = GameBoard::default();
+        let mut game_board = GameBoard::new();
         let o = &Piece::O;
 
-        let result = place_piece(o, (0, 0), &mut game_board)
-            .and_then(|_| place_piece(o, (0, 1), &mut game_board))
-            .and_then(|_| place_piece(o, (0, 2), &mut game_board));
+        let result = game_board
+            .place_piece(o, (0, 0))
+            .and_then(|_| game_board.place_piece(o, (0, 1)))
+            .and_then(|_| game_board.place_piece(o, (0, 2)));
 
         assert_eq!(result, Ok(()));
 
@@ -194,60 +204,62 @@ mod tests {
 
     #[test]
     fn test_empty_board_has_no_winner() {
-        let game_board = GameBoard::default();
+        let game_board = GameBoard::new();
 
-        assert_eq!(None, determine_winner(&game_board));
+        assert_eq!(None, game_board.determine_winner());
     }
 
     #[test]
     fn test_board_with_a_winning_row_has_a_winner() {
-        let mut game_board = GameBoard::default();
+        let mut game_board = GameBoard::new();
         let x = &Piece::X;
 
-        let result = place_piece(x, (0, 0), &mut game_board)
-            .and_then(|_| place_piece(x, (0, 1), &mut game_board))
-            .and_then(|_| place_piece(x, (0, 2), &mut game_board));
+        let result = game_board
+            .place_piece(x, (0, 0))
+            .and_then(|_| game_board.place_piece(x, (0, 1)))
+            .and_then(|_| game_board.place_piece(x, (0, 2)));
 
         assert!(result.is_ok());
-        assert_eq!(Some(x.to_owned()), determine_winner(&game_board));
+        assert_eq!(Some(x.to_owned()), game_board.determine_winner());
     }
 
     #[test]
     fn test_board_with_a_winning_column_has_a_winner() {
-        let mut game_board = GameBoard::default();
+        let mut game_board = GameBoard::new();
         let x = &Piece::X;
 
-        let result = place_piece(x, (0, 0), &mut game_board)
-            .and_then(|_| place_piece(x, (1, 0), &mut game_board))
-            .and_then(|_| place_piece(x, (2, 0), &mut game_board));
+        let result = game_board
+            .place_piece(x, (0, 0))
+            .and_then(|_| game_board.place_piece(x, (1, 0)))
+            .and_then(|_| game_board.place_piece(x, (2, 0)));
 
         assert!(result.is_ok());
-        assert_eq!(Some(x.to_owned()), determine_winner(&game_board));
+        assert_eq!(Some(x.to_owned()), game_board.determine_winner());
     }
 
     #[test]
     fn test_board_with_a_winning_forward_diagonal_has_a_winner() {
-        let mut game_board = GameBoard::default();
+        let mut game_board = GameBoard::new();
         let x = &Piece::X;
 
         for index in 0..=2 {
-            _ = place_piece(x, (index, index), &mut game_board);
+            _ = game_board.place_piece(x, (index, index));
         }
 
-        assert_eq!(Some(x.to_owned()), determine_winner(&game_board));
+        assert_eq!(Some(x.to_owned()), game_board.determine_winner());
     }
 
     #[test]
     fn test_board_with_a_winning_backward_diagonal_has_a_winner() {
-        let mut game_board = GameBoard::default();
+        let mut game_board = GameBoard::new();
         let x = &Piece::X;
 
         let backward_diagonal_indicies = (0..=2).zip((0..=2).rev()).enumerate();
 
         for (_, (row_index, column_index)) in backward_diagonal_indicies {
-            _ = place_piece(x, (row_index, column_index), &mut game_board);
+            _ = game_board.place_piece(x, (row_index, column_index));
         }
 
-        assert_eq!(Some(x.to_owned()), determine_winner(&game_board));
+        assert_eq!(Some(x.to_owned()), game_board.determine_winner());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 mod game_model;
 
-use crate::game_model::{determine_winner, place_piece, GameBoard, Piece};
+use crate::game_model::{GameBoard, Piece};
 
 fn main() {
-    let mut game_board: GameBoard = Default::default();
+    let mut game_board = GameBoard::new();
     println!("Hello game board: {game_board:#?}");
 
-    _ = place_piece(&Piece::O, (0, 0), &mut game_board);
-    println!("game_board[0]: {:#?}\n", game_board[0]);
+    _ = game_board.place_piece(&Piece::O, (0, 0));
+    println!("game_board[0]: {:#?}\n", game_board.cells[0]);
     println!("Row 0: ");
 
-    let mut game_board2: GameBoard = Default::default();
+    let mut game_board2 = GameBoard::new();
 
     let mut result = Err(String::new());
     for column in 0..=2 {
-        result = place_piece(&Piece::X, (0, column), &mut game_board2);
+        result = game_board2.place_piece(&Piece::X, (0, column));
     }
 
-    println!("game_board[0]: {:#?}\n", game_board2[0]);
+    println!("game_board[0]: {:#?}\n", game_board2.cells[0]);
 
     if let Err(error) = result {
         println!(
@@ -25,6 +25,6 @@ fn main() {
             &game_board2, error
         );
     } else {
-        println!("Winner: {:#?}", determine_winner(&game_board2));
+        println!("Winner: {:#?}", game_board2.determine_winner());
     }
 }
